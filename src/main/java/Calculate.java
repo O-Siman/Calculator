7f1a84c11611fefc448c2aa0b42c8c98422056bd
John Klay <johnandklay@gmail.com>
2020-10-22 18:03:39 -0400

    Added more things

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -1,3 +1,4 @@
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 import java.util.ArrayList;
@@ -7,13 +8,14 @@ public class Calculate {
     public static void calculate(GuildMessageReceivedEvent event) {
         //Set message content to a variable
         String message = event.getMessage().getContentRaw();
+        TextChannel channel = event.getChannel();
 
         //Remove the ` prefix
         message = message.substring(1);
 
         //Makes sure that no letters or special characters are used
         if (!message.matches("^[1234567890*x+-\\/\\s]+$")) {
-            event.getChannel().sendMessage("That's not a valid calculation.").queue();
+            channel.sendMessage("**Invalid Syntax:** A calculation can only include numbers and operations (`+`, `-`, `*` or `x`, `/`).").queue();
             return;
         }
 
@@ -23,30 +25,76 @@ public class Calculate {
         //Since numbers can be more than double digits, log the number
         String currentNumber = "";
 
+        //List of all numbers in the operation
+        ArrayList<String> listOfNumbers = new ArrayList<>();
+
+        //List of all operations
+        ArrayList<Character> listOfOperations = new ArrayList<>();
+
+        //In case there are two operations in a row
+        boolean previousCharIsOperation = false;
+
         //Calculation loop starts here. Will loop through every character in message
         for (int i = 0; i < message.length(); i++) {
 
             //"Upgrade" from primitive type to whatever this is
-            Character currentChar = message.charAt(0);
+            Character currentChar = message.charAt(i);
 
             //if we are on the first character
             if (i == 0) {
                 //And if the character is not a number
                 if (!currentChar.toString().matches("[1234567890]")) {
-                    event.getChannel().sendMessage("Your calculation must start with a number.").queue();
-                    return;
+                    channel.sendMessage("**Invalid syntax:** Your calculation must start with a number.").queue();
+                    continue;
                 }
             }
 
             //If it is a calculation
-            if (currentChar.toString().matches("[\\/*-+x]")) {
+            if (currentChar.toString().matches("[\\/*\\-+x]")) {
+                //If this is the second operation in a row
+                if (previousCharIsOperation) {
+                    channel.sendMessage("**Invalid syntax:** You can't have two operations in a row!").queue();
+                    continue;
+                }
+                //Valid operation
+
+                //Set to true because this char is an operation
+                previousCharIsOperation = true;
+
+                //Log completed number in array
+                listOfNumbers.add(currentNumber);
+                //Reset currentNumber
+                currentNumber = "";
+                //Log operation in array
+                listOfOperations.add(currentChar);
+
                 //Flag as addition, subtraction, mult, div. Then perform calculation outside of for loop
-                return;
+                continue;
             }
+
+            //Mark as false, because this char is a number
+            previousCharIsOperation = false;
 
             //If it is a number, log the number
             currentNumber = currentNumber + currentChar.toString();
 
+        }
+        //Add final number to list
+        listOfNumbers.add(currentNumber);
+
+//        //Reset currentNumber (not necessary)
+//        currentNumber = "";
+
+        //TODO: Debug
+        channel.sendMessage("List of all numbers in this equation:").queue();
+        for (int i = 0; i < listOfNumbers.size(); i++) {
+            channel.sendMessage(listOfNumbers.get(i)).queue();
+        }
+
+        //TODO: Debug
+        channel.sendMessage("List of all operations in this equation:").queue();
+        for (int i = 0; i < listOfOperations.size(); i++) {
+            channel.sendMessage(listOfOperations.get(i).toString()).queue();
         }
 
     }
