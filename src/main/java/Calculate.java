1b31090a0b48f72f9fa54227276d82278e599211
John Klay <johnandklay@gmail.com>
2020-10-23 13:32:20 -0400

    Fixed an Exception with using too many operations

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -14,7 +14,7 @@ public class Calculate {
         message = message.substring(1);
 
         //Makes sure that no letters or special characters are used
-        if (!message.matches("^[1234567890*x+-\\/\\s]+$")) {
+        if (!message.matches("^[1234567890*x+\\-\\/.\\s]+$")) {
             channel.sendMessage("**Invalid Syntax:** A calculation can only include numbers and operations (`+`, `-`, `*` or `x`, `/`).").queue();
             return;
         }
@@ -26,7 +26,7 @@ public class Calculate {
         String currentNumber = "";
 
         //List of all numbers in the operation
-        ArrayList<String> listOfNumbers = new ArrayList<>();
+        ArrayList<Double> listOfNumbers = new ArrayList<>();
 
         //List of all operations
         ArrayList<Character> listOfOperations = new ArrayList<>();
@@ -34,7 +34,7 @@ public class Calculate {
         //In case there are two operations in a row
         boolean previousCharIsOperation = false;
 
-        //Calculation loop starts here. Will loop through every character in message
+        //Parsing loop starts here. Will loop through every character in message
         for (int i = 0; i < message.length(); i++) {
 
             //"Upgrade" from primitive type to whatever this is
@@ -43,9 +43,9 @@ public class Calculate {
             //if we are on the first character
             if (i == 0) {
                 //And if the character is not a number
-                if (!currentChar.toString().matches("[1234567890]")) {
+                if (!currentChar.toString().matches("[1234567890.]")) {
                     channel.sendMessage("**Invalid syntax:** Your calculation must start with a number.").queue();
-                    continue;
+                    return;
                 }
             }
 
@@ -54,21 +54,21 @@ public class Calculate {
                 //If this is the second operation in a row
                 if (previousCharIsOperation) {
                     channel.sendMessage("**Invalid syntax:** You can't have two operations in a row!").queue();
-                    continue;
+                    return;
                 }
-                //Valid operation
+                //Only reaches here if it is a valid operation
 
                 //Set to true because this char is an operation
                 previousCharIsOperation = true;
 
                 //Log completed number in array
-                listOfNumbers.add(currentNumber);
+                listOfNumbers.add(Double.parseDouble(currentNumber));
                 //Reset currentNumber
                 currentNumber = "";
                 //Log operation in array
                 listOfOperations.add(currentChar);
 
-                //Flag as addition, subtraction, mult, div. Then perform calculation outside of for loop
+                //Next character
                 continue;
             }
 
@@ -79,23 +79,89 @@ public class Calculate {
             currentNumber = currentNumber + currentChar.toString();
 
         }
+
         //Add final number to list
-        listOfNumbers.add(currentNumber);
+        try {
+            listOfNumbers.add(Double.parseDouble(currentNumber));
+        } catch (Exception e) {
+            //If there too many operations vs numbers (such as in example: 8 + 2 - 3 *)
+            channel.sendMessage("**Invalid Syntax:** You have too many operations.").queue();
+            return;
+        }
 
 //        //Reset currentNumber (not necessary)
 //        currentNumber = "";
+/*
 
-        //TODO: Debug
         channel.sendMessage("List of all numbers in this equation:").queue();
         for (int i = 0; i < listOfNumbers.size(); i++) {
-            channel.sendMessage(listOfNumbers.get(i)).queue();
+            channel.sendMessage(listOfNumbers.get(i).toString()).queue();
         }
 
-        //TODO: Debug
         channel.sendMessage("List of all operations in this equation:").queue();
         for (int i = 0; i < listOfOperations.size(); i++) {
             channel.sendMessage(listOfOperations.get(i).toString()).queue();
         }
+*/
 
+        /*Parse all operations to integer IDs
+        IDs for Math
+        Addition (+) = 0
+        Subtraction (-) = 1
+        Multiplication (* or x) = 2
+        Division (/) = 3
+         */
+
+        //Make a new ArrayList that will hold IDs of the operation instead of characters
+        ArrayList<Integer> listOfOperationIDs = new ArrayList<>();
+
+        //Loop through all characters in listOfOperations
+        for (int i = 0; i < listOfOperations.size(); i++) {
+            switch (listOfOperations.get(i)) {
+                case '+': listOfOperationIDs.add(0); break;
+                case '-': listOfOperationIDs.add(1); break;
+                case '*': listOfOperationIDs.add(2); break;
+                case 'x': listOfOperationIDs.add(2); break;
+                case '/': listOfOperationIDs.add(3); break;
+            }
+        }
+
+        //Start calculating here
+
+
+        double firstNumber;
+        double secondNumber;
+        int operation;
+
+        firstNumber = listOfNumbers.get(0);
+
+        //Order of events that must happen: Set second number, set operation, calculate set firstNumber to result.
+        for (int i = 0; i < listOfNumbers.size() - 1; i++) {
+            double result = 0;
+            secondNumber = listOfNumbers.get(i + 1);
+            operation = listOfOperationIDs.get(i);
+
+            //Check what operation it is
+            switch (operation) {
+                //If it's addition, add the numbers
+                case 0: result = firstNumber + secondNumber; break;
+                case 1: result = firstNumber - secondNumber; break;
+                case 2: result = firstNumber * secondNumber; break;
+                //Division, so we must make sure you don't divide by 0
+                case 3: {
+                    if (secondNumber == 0) {
+                        channel.sendMessage("**Invalid Calculation:** Expression involved dividing by zero").queue();
+                        return;
+                    }
+                    result = firstNumber / secondNumber;
+                    break;
+                }
+            }
+
+            firstNumber = result;
+        }
+
+        channel.sendMessage("Result: **" + firstNumber + "**").queue();
+//        System.out.println("Result is " + firstNumber);
     }
 }
